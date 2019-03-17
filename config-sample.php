7aac96229a2a03140e72a164647712854f5d5064
Marcin Snoch <marcin.msx@gmail.com>
2019-03-17 16:39:05 +0100

    Work in progress

---
+++ config-sample.php
@@ -0,0 +1,9 @@
+<?php
+
+$mailConfig = [
+    'host' => 'smtp.gmail.com',
+    'user' => 'xxx@gmail.com',
+    'password' => 'password',
+    'port' => 587,
+    'recipient' => ['email@example.com', 'WebSite'],
+];
